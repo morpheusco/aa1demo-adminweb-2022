b91ac5150a65af8e6fb54a4caf83777ad64820cd
Edison Monsalve Cuartas <edison.monsalve@gmail.com>
2022-10-10 14:44:15 -0500

    DemoAA1-AdminWeb-2022

---
+++ assets/js/cotecmar.js
@@ -0,0 +1,11 @@
+$(window).scroll(function () {
+
+    var nav = $('.barra_superior');
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+        nav.addClass("fondo-menu");
+    } else {
+        nav.removeClass("fondo-menu");
+    }
+});
